5f35db544e59ad9807d921a39f1f0c068e150b6f
Alkima-hydra <latinoignas@gmail.com>
2026-02-10 18:15:55 -0400

    arrreglitos

--- src/pages/signin/SignInMain.js
+++ src/pages/signin/SignInMain.js
@@ -9,7 +9,7 @@ import iconImg from '../../assets/img/contact/Icon.png';
 const SignInMain = () => {
   return (
     <main>
-      <Breadcrumb title="Sign in" />
+      <Breadcrumb title="Iniciar Sesión" />
 
       <div className="it-signup-area pt-120 pb-120">
         <div className="container">
@@ -21,18 +21,18 @@ const SignInMain = () => {
               <div className="col-xl-6 col-lg-6">
                 <form action="#">
                   <div className="it-signup-wrap">
-                    <h4 className="it-signup-title">sign In</h4>
+                    <h4 className="it-signup-title">Iniciar Sesión</h4>
                     <div className="it-signup-input-wrap">
                       <div className="it-signup-input mb-20">
-                        <input type="email" placeholder="Email *" />
+                        <input type="email" placeholder="Correo Electrónico *" />
                       </div>
                       <div className="it-signup-input mb-20">
-                        <input type="password" placeholder="Password *" />
+                        <input type="password" placeholder="Contraseña *" />
                       </div>
                     </div>
                     <div className="it-signup-forget d-flex justify-content-between flex-wrap">
                       <a className="mb-20" href="#">
-                        Forgot Password?
+                        Olvidaste tu contraseña?
                       </a>
                       <div className="it-signup-agree mb-20">
                         <div className="form-check">
@@ -46,28 +46,23 @@ const SignInMain = () => {
                             className="form-check-label"
                             for="flexCheckDefault"
                           >
-                            Remember me
+                            Manten mi sesión iniciada
                           </label>
                         </div>
                       </div>
                     </div>
                     <div className="it-signup-btn d-sm-flex justify-content-between align-items-center mb-40">
                       <button type="submit" className="ed-btn-theme">
-                        sign in
+                        Iniciar Sesión
                         <i>
                           <RightArrow />
                         </i>
                       </button>
-                      <div className="it-signup-link">
-                        <span>or sign up with</span>
-                        <a href="#">
-                          <img src={iconImg} alt="" />
-                        </a>
-                      </div>
+                      
                     </div>
                     <div className="it-signup-text">
                       <span>
-                        Don't have an account? <Link to="/signup">Sign Up</Link>
+                        ¿No tienes una cuenta? <Link to="/student-registration">Regístrate</Link>
                       </span>
                     </div>
                   </div>
--- src/pages/student/StudentRegistrationMain.js
+++ src/pages/student/StudentRegistrationMain.js
@@ -23,17 +23,17 @@ const StudentRegistrationMain = () => {
 
   return (
     <main>
-      <Breadcrumb title="Student Registration" subTitle="Registration" />
+      <Breadcrumb title="Registro de Estudiante" subTitle="Registro" />
 
       <div className="it-student-area pt-120 pb-120">
         <div className="container">
           <div className="row">
             <div className="col-xl-12">
               <div className="it-student-bg">
-                <h4 className="it-student-title">Student Registration</h4>
+                <h4 className="it-student-title">Registro de Estudiante</h4>
                 <div className="it-student-content mb-70">
                   <h4 className="it-student-subtitle">
-                    Fields with are required
+                    Los campos con * son obligatorios
                   </h4>
                   <p>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
@@ -42,15 +42,15 @@ const StudentRegistrationMain = () => {
                   </p>
                 </div>
                 <div className="it-student-regiform">
-                  <h4 className="it-student-regiform-title">Credentials</h4>
+                  <h4 className="it-student-regiform-title">Credenciales</h4>
                   <form ref={formRef} onSubmit={handleSubmit}>
                     <div className="it-student-regiform-wrap">
                       <div className="it-student-regiform-item">
-                        <label>First Name *</label>
+                        <label>Nombre *</label>
                         <input type="text" />
                       </div>
                       <div className="it-student-regiform-item">
-                        <label>Last Name *</label>
+                        <label>Apellidos *</label>
                         <input type="text" />
                       </div>
                       <div className="it-student-regiform-item">
@@ -62,11 +62,11 @@ const StudentRegistrationMain = () => {
                         <input type="email" />
                       </div>
                       <div className="it-student-regiform-item">
-                        <label>Password *</label>
+                        <label>Contraseña *</label>
                         <input type="text" />
                       </div>
                       <div className="it-student-regiform-item">
-                        <label>Password Confirmation *</label>
+                        <label>Confirmación de Contraseña *</label>
                         <input type="text" />
                       </div>
                       <div className="it-student-regiform-btn">
@@ -74,7 +74,7 @@ const StudentRegistrationMain = () => {
                           className="ed-btn-square purple-4 w-100"
                           type="submit"
                         >
-                          <span>Create account</span>
+                          <span>Crear cuenta</span>
                         </button>
                       </div>
                     </div>
